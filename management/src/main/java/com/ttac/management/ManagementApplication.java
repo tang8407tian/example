5ed1c9ebca4609e9f88082fcccce883db7b4d38c
bang.tang <bang.tang@biz-united.com.cn>
2018-10-15 17:04:02 +0800

    add

--- management/src/main/java/com/ttac/management/ManagementApplication.java
+++ management/src/main/java/com/ttac/management/ManagementApplication.java
@@ -3,6 +3,9 @@ package com.ttac.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author fish
+ */
 @SpringBootApplication
 public class ManagementApplication {
 
